c29b357ebcebb16a46e91a991c09eff3eb700e1c
Stanislaw Cruz <stanycruz@outlook.com>
2025-07-30 09:12:29 -0300

    feat: add pages

---
+++ src/pages/Grocery.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Grocery() {
+  return (
+    <div>
+      <h1>Grocery</h1>
+    </div>
+  )
+}
+
+export default Grocery
---
+++ src/pages/Owner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Owner() {
+  return (
+    <div>
+      <h1>Owner</h1>
+    </div>
+  )
+}
+
+export default Owner
