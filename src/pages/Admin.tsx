603604ccf64de9f93c4e20bec205bf58a3ddc8ed
Stanislaw Cruz <stanycruz@outlook.com>
2025-07-30 14:16:56 -0300

    feat: update admin page

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import api from '../services/api';
 
-function Admin() {
+export default function Admin() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    api.get('/v1/admin')
+      .then((res) => setMessage(res.data))
+      .catch(() => setMessage('Acesso negado ou erro na requisição'));
+  }, []);
+
   return (
     <div>
-      <h1>Área restrita: Admin</h1>
+      <h1>Admin</h1>
+      <p>{message}</p>
     </div>
-  )
+  );
 }
-
-export default Admin
