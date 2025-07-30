4efdae1d0c471b86277d27ff968800e4727a6d27
Stanislaw Cruz <stanycruz@outlook.com>
2025-07-30 14:22:34 -0300

    feat: update grocery page

--- src/pages/Grocery.tsx
+++ src/pages/Grocery.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import api from '../services/api';
 
-function Grocery() {
+export default function Grocery() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    api.get('/v1/grocery')
+      .then((res) => setMessage(res.data))
+      .catch(() => setMessage('Acesso negado ou erro na requisição'));
+  }, []);
+
   return (
     <div>
       <h1>Grocery</h1>
+      <p>{message}</p>
     </div>
-  )
+  );
 }
-
-export default Grocery
