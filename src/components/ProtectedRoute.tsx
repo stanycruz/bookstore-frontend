c4ae0f6e9f2ea150cf81abf8cb1e5860fd510ad5
Stanislaw Cruz <stanycruz@outlook.com>
2025-07-30 08:43:10 -0300

    feat: add component protected route

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { Navigate } from 'react-router-dom';
+
+type Props = {
+  roles: string[];
+  children: React.ReactNode;
+};
+
+export function ProtectedRoute({ roles, children }: Props) {
+  const tokenParsed = keycloak.tokenParsed;
+
+  const userRoles = (tokenParsed?.realm_access?.roles || []) as string[];
+  const hasAccess = roles.some(role => userRoles.includes(role));
+
+  if (!hasAccess) return <Navigate to="/" replace />;
+
+  return <>{children}</>;
+}
