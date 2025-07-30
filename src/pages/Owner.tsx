30e00e7e69828d1af5b0c6afc6073a1d7541c9df
Stanislaw Cruz <stanycruz@outlook.com>
2025-07-30 14:21:23 -0300

    feat: update owner page

--- src/pages/Owner.tsx
+++ src/pages/Owner.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import api from '../services/api';
 
-function Owner() {
+export default function Owner() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    api.get('/v1/owner')
+      .then((res) => setMessage(res.data))
+      .catch(() => setMessage('Acesso negado ou erro na requisição'));
+  }, []);
+
   return (
     <div>
       <h1>Owner</h1>
+      <p>{message}</p>
     </div>
-  )
+  );
 }
-
-export default Owner
