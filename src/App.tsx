dfc4c5b51a42e226c0a435832ad7bd531eb9b7fb
Stanislaw Cruz <stanycruz@outlook.com>
2025-07-31 15:52:26 -0300

    feat: add global layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,64 +5,66 @@ import Grocery from './pages/Grocery';
 import Maintainer from './pages/Maintainer';
 import Rookie from './pages/Rookie';
 import Unauthorized from './pages/Unauthorized';
-import { ProtectedRoute } from './routes/ProtectedRoute';
-import Navbar from './components/Navbar';
-import Dashboard from './components/Dashboard';
 import NotFound from './pages/NotFound';
+import Dashboard from './components/Dashboard';
+import { ProtectedRoute } from './routes/ProtectedRoute';
+import Layout from './layouts/Layout';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute roles={['admin', 'owner', 'maintainer', 'rookie']}>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute roles={['admin']}>
-              <Admin />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/owner"
-          element={
-            <ProtectedRoute roles={['admin', 'owner']}>
-              <Owner />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/grocery"
-          element={
-            <ProtectedRoute roles={['owner', 'grocery', 'maintainer']}>
-              <Grocery />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/maintainer"
-          element={
-            <ProtectedRoute roles={['grocery', 'maintainer']}>
-              <Maintainer />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/rookie"
-          element={
-            <ProtectedRoute roles={['rookie']}>
-              <Rookie />
-            </ProtectedRoute>
-          }
-        />
+        <Route element={<Layout />}>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute roles={['admin', 'owner', 'maintainer', 'rookie']}>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute roles={['admin']}>
+                <Admin />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/owner"
+            element={
+              <ProtectedRoute roles={['admin', 'owner']}>
+                <Owner />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/grocery"
+            element={
+              <ProtectedRoute roles={['owner', 'grocery', 'maintainer']}>
+                <Grocery />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/maintainer"
+            element={
+              <ProtectedRoute roles={['grocery', 'maintainer']}>
+                <Maintainer />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/rookie"
+            element={
+              <ProtectedRoute roles={['rookie']}>
+                <Rookie />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+
         <Route path="/unauthorized" element={<Unauthorized />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
